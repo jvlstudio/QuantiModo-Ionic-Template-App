8176eb6dd1872131d93ca850ad9863eefa7f3a04
Kashif Zaidi <kashifzaidi@hotmail.com>
2015-11-23 17:07:28 +0500

    intermediate commit

--- platforms/ios/www/js/filters/fromNow.js
+++
@@ -1,8 +0,0 @@
-angular.module('starter')
-	// returns human readable age i.e 10 minutes ago
-	.filter('fromNow', function(){
-	    return function(value){
-	        var d = new Date(value * 1000);
-	        return moment(d).fromNow();
-	    }
-	});
